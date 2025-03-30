a930f16a1dfc99df356e6dc3ddbe52fde16531ba
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-30 23:56:10 +0530

    [Divyansh Garg] ADD: Implemented Custom Exception

---
+++ src/main/java/com/bridgelabz/customexception/AgeValidator.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.customexception;
+
+
+import java.util.Scanner;
+
+// Custom Exception Class
+class InvalidAgeException extends Exception {
+    public InvalidAgeException() {
+        super("Age must be 18 or above");
+    }
+}
+
+public class AgeValidator {
+    // Method to validate age
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException();
+        }
+        System.out.println("Access granted!");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+            validateAge(age);
+        }
+        catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
