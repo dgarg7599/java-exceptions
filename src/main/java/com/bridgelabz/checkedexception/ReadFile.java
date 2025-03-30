f53b3d90ef10c7820d18ef385c645f5a941b259a
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-30 19:27:08 +0530

    [Divyansh Garg] ADD: Added java-exceptions

---
+++ src/main/java/com/bridgelabz/checkedexception/ReadFile.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.checkedexception;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        File file = new File("D:\\bridgelabz-workspace\\java-exceptions\\src\\main\\java\\com\\bridgelabz\\checkedexception\\data.txt");
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());;
+        }
+    }
+}
